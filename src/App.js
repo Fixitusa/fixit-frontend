27a72dd81c5aa9e7197eff9671b0788520e2a5fb
Fixitusa <Shawn.Blair@Myfixitusa.com>
2025-08-02 22:26:05 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div>
+ >
+          <img
+            src="/logo.png"
+            alt="FixIt Logo"
+            style={{ height: '80px', marginRight: '1rem' }}
+          />
+
       <header style={{backgroundColor: '#0080d6', color: '#fff', padding: '1rem'}}>FixIt USA</header>
       <main style={{padding: '1rem'}}>Welcome to FixIt SaaS!</main>
     </div>
